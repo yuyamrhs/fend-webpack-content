55372028946242a0e0e8ec8fcfeba59dac06efbb
yuyamrhs <yuya.mrhs1@gmail.com>
2020-04-23 16:13:58 -0700

    step-6_done

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,7 @@ const path = require("path")
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     mode: "development",
@@ -28,6 +29,7 @@ module.exports = {
             // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
-        })
+        }),
+        new BundleAnalyzerPlugin()
     ]
 }
